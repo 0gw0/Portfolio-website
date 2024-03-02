990932470e3521485c9d4d960eeea7ff9e8ea20c
0Ogw <glen.wang2709@gmail.com>
2024-03-02 12:04:55 +0800

    basic styling for index and nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,16 @@
 import Link from 'next/link';
 
 const Nav = () => (
-	<nav>
-		<Link href="/">Home</Link>
-		<Link href="/projects">Projects</Link>
-		<Link href="/contact">Contact</Link>
+	<nav className="text-center space-x-8 m-4 text-lg">
+		<Link href="/" className="hover:underline">
+			Home
+		</Link>
+		<Link href="/projects" className="hover:underline">
+			Projects
+		</Link>
+		<Link href="/contact" className="hover:underline">
+			Contact
+		</Link>
 	</nav>
 );
 
