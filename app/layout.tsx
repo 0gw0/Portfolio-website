a3c2eed90d4436537faf7553e9ccf64511032a30
0Ogw <glen.wang2709@gmail.com>
2024-05-02 01:31:55 +0800

    Add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Header from '@/components/header';
 import ActiveSectionContextProvider from '@/context/active-section-context';
 import { Toaster } from 'react-hot-toast';
+import Footer from '@/components/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
 				<ActiveSectionContextProvider>
 					<Header />
 					{children}
+					<Footer />
 					<Toaster position="bottom-right" />
 				</ActiveSectionContextProvider>
 			</body>
