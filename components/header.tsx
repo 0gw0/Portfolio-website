ea89f1bc213e104f962a1d33860e9b0a5dffd700
0Ogw <glen.wang2709@gmail.com>
2024-05-03 00:50:58 +0800

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -32,7 +32,7 @@ export default function Header() {
 								className={clsx(
 									'flex w-full items-center justify-center p-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300',
 									{
-										'text-gray-950 dark:text-gray-200':
+										'text-gray-950 dark:text-gray-100/70':
 											activeSection === link.name,
 									}
 								)}
