8b51135e3feda2c10c70f41aae4be402124a0e9d
0Ogw <glen.wang2709@gmail.com>
2024-09-13 19:31:12 +0800

    Update Projects page
    Back button sends user back to the project section

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,20 +1,32 @@
+'use client';
+
 import React from 'react';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { projectsData } from '@/lib/data';
 import ProjectCard from '@/components/projectCard';
 import { IoArrowBack } from 'react-icons/io5';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 export default function ProjectsPage() {
+	const router = useRouter();
+	const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+
+	const handleBackClick = () => {
+		setActiveSection('Projects');
+		setTimeOfLastClick(Date.now());
+		router.push('/#projects');
+	};
+
 	return (
 		<div className="bg-slate-50 dark:bg-zinc-900 min-h-screen">
 			<header className="bg-white dark:bg-zinc-800 py-8 px-4 shadow-md">
 				<div className="container mx-auto relative">
-					<Link
-						href="/"
+					<button
+						onClick={handleBackClick}
 						className="absolute top-0 left-0 flex items-center px-4 py-2 rounded-full bg-slate-200 dark:bg-zinc-700 text-gray-800 dark:text-gray-200 text-sm hover:bg-slate-300 dark:hover:bg-zinc-600 transition-all duration-300"
 					>
 						<IoArrowBack className="mr-2" /> Back to Home
-					</Link>
+					</button>
 					<h1 className="text-4xl md:text-5xl font-bold text-center mt-8 text-gray-950 dark:text-gray-200">
 						My Projects
 					</h1>
